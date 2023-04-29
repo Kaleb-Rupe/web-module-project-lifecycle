de9712227599bbe343c4a8276868be0819edd170
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-04-29 00:19:15 -0400

    getting todos from server

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { Component } from "react";
 
-export default class Form extends React.Component {
+export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      form: [],
+    };
+  }
+
   render() {
-    return null
+    return null;
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from "react";
 
-export default class TodoList extends React.Component {
+export default class TodoList extends Component {
   render() {
-    return null
+    return null;
   }
 }
